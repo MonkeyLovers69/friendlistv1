3fdcbbff3bf2f476f093f07f9c6b697b32eeafc5
MonkeyLovers69 <137757068+MonkeyLovers69@users.noreply.github.com>
2023-07-06 00:53:04 +0500

    Add files via upload

---
+++ main (13).py
@@ -0,0 +1,15 @@
+friend_list= {
+    "anvar":"14",
+    "sasha":"13",
+    "artem":"12"
+}
+print(friend_list)
+del friend_list["anvar"]
+print(friend_list)
+friend_list= {
+    "anvar":"14",
+    "sasha":"13",
+    "artem":"12"
+}
+friend_list["johny"] = "15"
+print(friend_list)
